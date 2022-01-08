0ed494ef268d4941ec3c8e0dcd2fb1788cc33fe5
maklad42 <maklad42+git@gmail.com>
2022-01-08 22:19:17 +0900

    Adds progerss bar and fixes styled components flicker on load. 220108

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,11 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeDone', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
