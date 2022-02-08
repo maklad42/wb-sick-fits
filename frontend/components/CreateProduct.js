b4d19941417be97473f6c44cc2f7688c40774818
maklad42 <maklad42+git@gmail.com>
2022-02-08 21:27:40 +0900

    Adds refetching of the successful query and redirecting to product page. 220208

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,7 +1,9 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import DisplayError from './ErrorMessage';
 import useForm from '../lib/useForm';
+import { ALL_PRODUCTS_QUERY } from './Products';
 import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
@@ -41,15 +43,15 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
-  console.log(createProduct);
   return (
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
-        // submit the input fields to the backend:
+        // Submit the inputfields to the backend:
         await createProduct();
         clearForm();
       }}
